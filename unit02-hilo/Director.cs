d666824d249bc6f7e7195ab4468ec8710d1f51b3
kai <kaidizhang@byui.edu>
2022-05-14 20:04:39 -0600

    final change

--- unit02-hilo/Director.cs
+++ unit02-hilo/Director.cs
@@ -9,8 +9,6 @@ namespace unit02_hilo
         bool isPlaying = true;
         int point = 0; 
         int totalPoints = 300;
-        Card card1 = new Card();
-        Card card2 = new Card();
         string guess = " ";
         public Director()
         {
@@ -21,19 +19,18 @@ namespace unit02_hilo
             while (isPlaying)
             {
                 GetInputs();
-                DoUpdates();
                 GetOutputs();
             }
         }
         public void GetInputs()
-        {
+        {   
+            Card card1 = new Card();
+            Card card2 = new Card(); 
+            Console.WriteLine();
             Console.Write($"The card is: {card1.value}. ");
             Console.WriteLine();
             Console.Write("Higher or Lower? [h/l] ");
             guess = Console.ReadLine();
-        }
-        public void DoUpdates()
-        {
             Console.Write($"The new card is: {card2.value}. ");
             if (guess ==  "h" && card2.value >= card1.value)
             {
@@ -58,6 +55,11 @@ namespace unit02_hilo
             string playAgain = Console.ReadLine();
             if (playAgain == "n")
             {
+                isPlaying = false;
+            }
+            if (totalPoints <= 0)
+            {
+                Console.Write("Game over!!!");
                 isPlaying = false;
             }
         }
